36414d84cf2a4e9bad3d24cfc8f20f93f8ffe483
sahyunjin <tkguswls1106@naver.com>
2024-09-19 06:24:31 +0900

    Fix: starturl 변경

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,22 +27,8 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        if (response) {
-          return response; // 캐시에서 찾은 경우 반환
-        }
-
-        return fetch(event.request).then(networkResponse => {
-          // 네트워크 요청이 성공하면 캐시에 저장
-          if (networkResponse.status === 404) {
-            console.log("서비스워커 캐시 - 404")
-            return caches.match('/index.html'); // 404일 경우 기본 페이지 반환
-          }
-          return networkResponse;
-        }).catch(() => {
-          // 네트워크 요청이 실패할 경우 기본 페이지를 반환
-          console.log("서비스워커 실패 - ERROR");
-          return caches.match('/index.html');
-        });
+        // 캐시에서 찾으면 반환하고, 그렇지 않으면 네트워크 요청
+        return response || fetch(event.request);
       })
   );
 });
